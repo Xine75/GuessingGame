10427d3bf8a067a071cf23086615577cb59c0b1b
Xine <cjdoza@gmail.com>
2021-03-01 15:43:24 -0600

    phase1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string secretNumber = "42";
+            Console.WriteLine("Hello! Want to have some fun? Guess my secret number!");
+
+
+
+            string question = "What's your guess?";
+            Console.Write($"{question}: ");
+            string answer = Console.ReadLine();
+            Console.Write(answer);
+
+
+
         }
     }
 }
