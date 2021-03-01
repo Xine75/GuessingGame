fcfc6098f704f16c93b6055f4d6dd64928d0fb61
Xine <cjdoza@gmail.com>
2021-03-01 16:37:58 -0600

    infinite loop

--- Program.cs
+++ Program.cs
@@ -13,30 +13,24 @@ namespace GuessingGame
             string question = "What's your guess?";
 
             int num = 0;
-
-            while (num < 4)
             {
-
-                Console.Write($"{question}: ");
+                while (num < 4)
+                    Console.Write($"{question}: ");
                 string answer = Console.ReadLine();
+
                 if (answer != secretNumber)
                 {
-                    Console.WriteLine("Oh dear, I'm afraid you're wrong");
+                    Console.WriteLine($"Oh dear, I'm afraid you're wrong. You have tried {num + 1} times.");
+                    num++;
                 }
-                else
+                else if (answer == secretNumber)
                 {
-                    Console.WriteLine("Ding Dong! You got it, dummy!");
+                    Console.WriteLine($"Ding Dong! You got it, dummy!");
+
                 }
-                num++;
 
 
             }
-
-
-
-
-
-
 
 
         }
