401b385237cc31f418dc95ae3d7cdefb293c78ec
Xine <cjdoza@gmail.com>
2021-03-02 10:13:29 -0600

    phase 6

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@
 int secretNumber = new Random().Next(1, 100);
 Console.WriteLine(secretNumber);
 
-Console.WriteLine("Hello! Want to have some fun? Guess my secret number!");
+Console.WriteLine("Hello! Want to have some fun, dummy?");
 
 
-string question = "What's your guess, dummy?";
+string question = "Guess my secret number!";
 
 int num = 4;
 while (num > 0)
@@ -19,12 +19,20 @@ while (num > 0)
 
     if (parsedAnswer != secretNumber)
     {
-        Console.WriteLine($"Oh dear, I'm afraid you're wrong. You have {num - 1} more tries.");
-        num--;
+        if (parsedAnswer > secretNumber)
+        {
+            Console.WriteLine($"Oh dear, I'm afraid you're answer is too high. You have {num - 1} more tries.");
+            num--;
+        }
+        else
+        {
+            Console.WriteLine($"Oh dear, I'm afraid you're answer is too low. You have {num - 1} more tries.");
+            num--;
+        }
     }
     else if (parsedAnswer == secretNumber)
     {
-        Console.WriteLine($"Ding Dong! You got it!");
+        Console.WriteLine($"Nice job! You got it!");
         return;
     }
 
